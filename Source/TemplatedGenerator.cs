5258655582d1ed6d06851944070ebd230b3940d9
MrBretticus <mrbretticus@gmail.com>
2013-05-30 16:57:17 +0800

    Partial service/state interfaces

--- Source/TemplatedGenerator.cs
+++ Source/TemplatedGenerator.cs
@@ -110,8 +110,8 @@ public sealed class {0}";
                 if ((entity.Name ?? "default") == "default")
                     continue;
 
-                GenerateEntityInterface(entity, writer, "?", "public interface I{0}ApplicationService");
-                GenerateEntityInterface(entity, writer, "!", "public interface I{0}State");
+                GenerateEntityInterface(entity, writer, "?", "public partial interface I{0}ApplicationService");
+                GenerateEntityInterface(entity, writer, "!", "public partial interface I{0}State");
             }
         }
 
